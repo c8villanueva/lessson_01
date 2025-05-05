bbccaacfc5465e71b0005653b84766ca8850bb6f
c8villanueva <caitlin.villanueva@gmail.com>
2025-05-05 18:24:50 +0800

    test deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  'http://localhost:3000',
+  //'http://localhost:3000',
   // fake urls
-  'https://www.dandrepairshop.com',
-  'https://dandreparishop.com'
+  // 'https://www.dandrepairshop.com',
+  // 'https://dandreparishop.com',
+  'https://technotes-7wis.onrender.com/'
 ]
 
 module.exports = allowedOrigins
